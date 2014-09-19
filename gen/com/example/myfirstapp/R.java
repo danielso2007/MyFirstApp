e8319a230451c26c676624d662650f382f1130c2
Daniel Oliveira <danielso2007@gmail.com>
2014-09-19 12:41:05 -0300

    first commit

--- gen/com/example/myfirstapp/R.java
+++ gen/com/example/myfirstapp/R.java
@@ -31,10 +31,11 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_search=0x7f090003;
-        public static final int action_settings=0x7f090002;
+        public static final int action_search=0x7f090004;
+        public static final int action_settings=0x7f090003;
         public static final int container=0x7f090000;
         public static final int edit_message=0x7f090001;
+        public static final int textView1=0x7f090002;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -51,6 +52,7 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int button_send=0x7f060002;
         public static final int edit_message=0x7f060001;
+        public static final int label_sua_mensagem=0x7f060007;
         public static final int title_activity_display_message=0x7f060005;
         public static final int title_activity_main=0x7f060004;
     }
